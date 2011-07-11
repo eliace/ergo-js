b40b6f775706fa42177cee9e2a97dd87729f44f3
eliace <eliace@mail.ru>
2011-07-11 15:17:31 +0400

    data fix

---
+++ js/data/collection.js
@@ -0,0 +1,39 @@
+
+//= require <core/data-source>
+
+
+Dino.declare('Dino.data.Collection', 'Dino.core.DataSource', {
+	
+	defaults: {
+		itemModel: null
+	},
+	
+	
+	getByOID: function(oid) {
+		var a = this._val();
+		for(var i in a)
+			if(a[i].id == oid) return a[i];
+		return null;
+	},
+	
+	
+	factory: function(i) {
+		
+		/**
+		 * Фабрика должна создавать элементы с помощью функции-генератора класса.
+		 * Причем, могут быть такие случаи:
+		 *  - задана сама функция
+		 *  - задано имя класса
+		 *  - задано поле, которое содержит имя класса
+		 */
+		
+		var model = this.options.itemModel; //(this.options.itemModel) ? this.options.itemModel : Dino.core.DataSource;
+		if(Dino.isFunction(model)) model = model.call(this, this._val()[i]);
+		if(Dino.isString(model)) model = eval(model); //TODO здесь лучше загружать класс по зарегистрированному имени
+		model = model || Dino.core.DataSource;
+		return new model(this, i); 
+	}
+
+	
+	
+});
---
+++ js/data/model.js
@@ -0,0 +1,54 @@
+
+//= require <core/data-source>
+
+
+
+
+Dino.declare('Dino.data.Model', 'Dino.core.DataSource', {
+	
+	defaults: {
+		oidKey: 'id'
+	},
+	
+	fields: {
+	},
+	
+	oid: function() {
+		return this._val()[this.options.oidKey];
+	},
+	
+	
+	set: function(v) {
+		
+		if(this.validate) {
+			if( !this.validate.call(this, v) ) throw new Error('Invalid value: ['+v+']');
+		}
+		
+		Dino.data.Model.superclass.set.apply(this, arguments);
+	},
+	
+	get: function() {
+		var v = Dino.data.Model.superclass.get.apply(this, arguments);
+
+		return (this.format) ? this.format.call(this, v) : v;
+	},
+	
+	factory: function(i) {
+		
+		/**
+		 * Фабрика должна создавать элементы с помощью функции-генератора класса.
+		 * Причем, могут быть такие случаи:
+		 *  - задана сама функция
+		 *  - задано имя класса
+		 *  - задано поле, которое содержит имя класса
+		 */		
+		
+		var model = this.fields[i]
+		if(Dino.isFunction(model)) model = model.call(this, this._val()[i]);
+		if(Dino.isString(model)) model = eval(model); //TODO здесь лучше загружать класс по зарегистрированному имени
+		model = model || Dino.core.DataSource;
+		return new model(this, i);
+	}
+	
+});
+
