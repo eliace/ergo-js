42159f64de57bfd6a081b6eb5318e351cf528cd8
eliace <eliace@mail.ru>
2011-08-25 21:08:29 +0400

    + grouping layout sample

---
+++ samples/resources/js/layouts-grouping.js
@@ -0,0 +1,208 @@
+
+
+var listData = [
+	{text: 'Alice', id: 1, group: '1'},
+	{text: 'Charlie', id: 3, group: '2'},
+	{text: 'Dean', id: 4, group: '3'},
+	{text: 'Bob', id: 2, group: '1'},
+	{text: 'Eva', id: 5, group: '3'},
+];
+
+
+
+var groupData = [{
+	id: '1',
+	text: 'Группа 1',
+	users: [
+		{text: 'Alice', id: 1, group: '1'},
+		{text: 'Bob', id: 2, group: '1'},
+	]
+}, {
+	id: '2',
+	text: 'Группа 2',
+	users: [
+		{text: 'Charlie', id: 3, group: '2'},
+	]
+}, {
+	id: '3',
+	text: 'Группа 3',
+	users: [
+		{text: 'Dean', id: 4, group: '3'},
+		{text: 'Eva', id: 5, group: '3'},
+	]
+}];
+
+
+
+
+$.ergo({
+	etype: 'panel',
+	renderTo: '.preview',
+	width: 400,
+	title: 'Плоский список',
+	cls: 'dino-border-all',
+	content: {
+		etype: 'list',
+		data: listData,
+		dynamic: true,
+		cls: 'dino-text-content',
+		
+		defaultItem: {
+			etype: 'text-item',
+			style: {'display': 'block'},
+			binding: function(val) {
+				this.opt('text', val.text);
+				this.options.group = val.group;
+			}
+		}		
+	}
+	
+});
+
+
+
+
+var groupLayout = Ergo.layouts.StatefulLayout.extend({
+	
+/*	
+	insert: function(item) {
+		
+		if(!this.groups) this.groups = {};
+		
+		var g = item.data.get('group');
+		
+		if(!this.groups[g]) {
+			var el = $('<div class="group-title">Group '+g+'</div>');
+			this.groups[g] = el;
+			this.el.append(el);
+		}
+		
+		this.groups[g].after(item.el);
+		
+	}
+*/
+
+	
+	rebuild: function() {
+		
+		var c = this.container;
+		
+		if(!this.groups) this.groups = {};
+		
+		var self = this;
+		
+		for(var i = this.items.length-1; i--; i>= 0) {
+			
+			var item = this.items[i];
+			
+			var g = item.data.get('group');
+						
+			if(!self.groups[g]) {
+				var el = $('<div class="group-title">Hello</div>');
+				self.groups[g] = el;
+				self.el.append(el);
+			}
+
+			self.groups[g].after(item.el);			
+		}
+		
+		// Ergo.each(this.items, function(item){
+// 			
+		// });
+		
+		
+	}
+
+	
+});
+
+
+
+
+$.ergo({
+	etype: 'panel',
+	renderTo: '.preview',
+	width: 400,
+	title: 'Сгруппированный список',
+	cls: 'dino-border-all',
+	style: {'margin-top': 10},
+	content: {
+		etype: 'list',
+		data: listData,
+		dynamic: true,
+		cls: 'dino-text-content',
+		layout: new groupLayout,
+		
+		defaultItem: {
+			etype: 'text-item',
+			style: {'display': 'block'},
+			binding: function(val) {
+				this.opt('text', val.text);
+			}
+		}		
+	}
+	
+});
+
+
+
+
+/*
+var groupLayout2 = Ergo.layouts.PlainLayout.extend({
+	
+	insert: function(item) {
+
+		var parentItem = this.container.parent.parent;
+		
+		console.log(item.el);
+		
+		parentItem.el.after(item.el);
+		
+	}
+	
+	
+	
+});
+
+
+
+$.ergo({
+	etype: 'panel',
+	renderTo: '.preview',
+	width: 400,
+	title: 'Сгруппированный список',
+	cls: 'dino-border-all',
+	style: {'margin-top': 10},
+	content: {
+		etype: 'list',
+		data: groupData,
+		dynamic: true,
+		cls: 'dino-text-content',
+		
+		defaultItem: {
+			etype: 'text-item',
+			cls: 'group-title',
+			style: {'display': 'block'},
+			binding: function(val) {
+				this.opt('text', val.text);
+			},
+			components: {
+				groupItemList: {
+					dataId: 'users',
+					etype: 'list',
+					dynamic: true,
+					layout: new groupLayout2(),
+					defaultItem: {
+						etype: 'text-item',
+						style: {'display': 'block'},
+						binding: function(val) {
+							this.opt('text', val.text);
+						}						
+					}
+				}
+			}
+		}		
+	}
+	
+});
+*/
