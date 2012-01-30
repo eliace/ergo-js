05f45bb23e99e2e6fdd66b32d8b5966a18a5619e
Коданев Юрий <eliace.the.grey@gmail.com>
2012-01-30 18:21:14 +0400

    renaming

--- samples/js/2c-layout.js
+++ samples/js/2c-layout.js
@@ -1,30 +1,6 @@
 
 
 
-Ergo.declare('Ergo.layouts.TwoColumnLayout', 'Ergo.core.Layout', {
-	
-	defaults: {
-		html: '<div class="container" autoheight="ignore-siblings"><div class="content"/></div><aside class="left_bar" autoheight="ignore-siblings"/>'
-	},
-	
-	
-	select: function(item) {
-		if(item.options.region == 'sidebar') return this.el.filter('.left_bar');
-		return this.el.filter('.container').find('.content');
-	},
-	
-	
-	insert: function(item) {
-		this.$super.apply(this, arguments);
-		item.options.autoHeight = true;
-	}
-	
-	
-}, '2c-layout');
-
-
-
-
 
 
 $(document).ready(function(){
