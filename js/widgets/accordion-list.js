519942efeed7650ab4e7897446447adf216f0b03
eliace <eliace@mail.ru>
2012-04-01 17:28:33 +0300

    +accordion

---
+++ js/widgets/accordion-list.js
@@ -0,0 +1,43 @@
+
+
+Ergo.declare('Ergo.widgets.AccordionList', 'Ergo.widgets.List', {
+	
+	defaults: {
+		defaultItem: {
+			components: {
+				content: {
+					etype: 'text',
+					onClick: function() {
+						var p = this.parent;
+						p.parent.items.each(function(c){
+							if(c != p) c.states.clear('expanded');
+						});
+						p.states.toggle('expanded');
+					}
+				},
+				sublist: {
+					etype: 'list',
+					style: {'display': 'none'},
+					extensions: ['effects'],
+					effects: {
+						show: 'slideDown',
+						hide: 'slideUp',
+						delay: 300
+					}
+				}
+			},
+			states: {
+				'expanded': function(on) {
+					(on) ? this.sublist.show() : this.sublist.hide();
+				}
+			},
+			set: {
+				'text': function(v) {this.content.opt('text', v);}
+			}
+		}
+	}
+	
+	
+	
+}, 'accordion-list');
+
--- samples/js/trees-2.js
+++ samples/js/trees-2.js
@@ -2,41 +2,7 @@
 
 sample('Аккордеон', {
 	
-	etype: 'list',
-	
-	defaultItem: {
-		components: {
-			content: {
-				etype: 'text',
-				onClick: function() {
-					var p = this.parent;
-					p.parent.items.each(function(c){
-						if(c != p) c.states.clear('expanded');
-					});
-					p.states.toggle('expanded');
-				}
-			},
-			sublist: {
-				etype: 'list',
-				style: {'display': 'none'},
-				extensions: ['effects'],
-				effects: {
-					show: 'slideDown',
-					hide: 'slideUp',
-					delay: 300
-				}
-			}
-		},
-		states: {
-			'expanded': function(on) {
-				(on) ? this.sublist.show() : this.sublist.hide();
-			}
-		},
-		set: {
-			'text': function(v) {this.content.opt('text', v);}
-		}
-	},
-	
+	etype: 'accordion-list',	
 	
 	items: [{
 		text: 'Меню 1',
@@ -48,9 +14,5 @@ sample('Аккордеон', {
 		text: 'Меню 3',
 		components: {sublist: {items: ['субменю 1', 'субменю 2']}}
 	}]
-	
-	
-	
-	
 	
 });
