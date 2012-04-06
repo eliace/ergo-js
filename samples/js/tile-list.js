da30d81c50a1902fc88f96d606cb0202d8dee714
Коданев Юрий <eliace.the.grey@gmail.com>
2012-04-06 15:01:30 +0400

    + tile list

---
+++ samples/js/tile-list.js
@@ -0,0 +1,53 @@
+
+var listData = [{
+	img: 'img/worker_photo.png',
+	title: 'Волков В.П.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Зайце А.А.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Енотов С.Н.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Белкин В.С.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Медведев М.П.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Лось А.В.'
+}, {
+	img: 'img/worker_photo.png',
+	title: 'Росомахин И.Р.'
+}]
+
+
+
+sample('Список тайлов', {
+	
+	etype: 'list',
+	
+//	layout: 'float',
+	
+	data: listData,
+	
+	dynamic: true,
+	
+	defaultItem: {
+		width: 120,
+		height: 120,
+		style: {'background-color': '#ccc', 'float': 'left'},
+		components: {
+			image: {
+				etype: 'image',
+				dataId: 'img'
+			},
+			text: {
+				etype: 'text',
+				dataId: 'title'
+			}
+		}
+	}
+	
+});
