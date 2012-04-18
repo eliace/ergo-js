33c74c0494e1462391d1cae2b6f444c60b872eed
eliace <eliace@mail.ru>
2012-04-18 07:13:27 +0300

    ~

--- samples/js/scrollbar.js
+++ samples/js/scrollbar.js
@@ -21,13 +21,14 @@ sample('Пользовательский скроллбар', {
 				data: listData,
 				
 				dynamic: true
-				
-//				height: 8000
+								
 			},
 			scrollbar: {
 				cls: 'e-scrollbar-holder',
 				content: {
-					cls: 'e-scrollbar',
+					content: {
+						cls: 'e-scrollbar'
+					},
 					
 					events: {
 						'mousedown': function(e, w) {
@@ -38,9 +39,18 @@ sample('Пользовательский скроллбар', {
 								})
 								.on('mousemove', function(e){
 									var y = e.pageY - w.parent.el.offset().top - w._dy;
+									
+									var max_y = w.parent.el.height() - w.el.outerHeight();
+									
+									w.parent.parent.scrollContent(y / max_y);
+									w.parent.parent.scrollScrollbar(y / max_y);
+/*									
 									y = Math.max(0, y);
-									y = Math.min(w.parent.el.height() - w.el.height(), y);
+									y = Math.min(max_y, y);
 									w.el.css('margin-top', y);
+									
+									w.events.bubble('scrollerMove', {offset: y, ratio: y/max_y});
+*/									
 								})
 
 							e.preventDefault();
@@ -53,10 +63,17 @@ sample('Пользовательский скроллбар', {
 		
 		events: {
 			'mousewheel': function(e, delta, deltaX, deltaY, w) {
-				var y = parseInt(w.scrollbar.content.el.css('margin-top').replace("px", ""));
+//				var y = parseInt(w.scrollbar.content.el.css('margin-top').replace("px", ""));
+				var y = - parseInt(w.content.el.css('margin-top').replace("px", ""));
 				
-				y -= deltaY*8;
-
+				y -= deltaY*20;
+				
+				var max_y = w.content.el.outerHeight() - w.el.height();
+				
+				w.scrollContent(y / max_y);
+				w.scrollScrollbar(y / max_y);
+				
+/*
 				var max_y = w.scrollbar.el.height() - w.scrollbar.content.el.height();
 				
 				y = Math.max(0, y);
@@ -70,7 +87,7 @@ sample('Пользовательский скроллбар', {
 				y = (w.content.el.height() - w.el.height()) * ratio;
 				
 				w.content.el.css('margin-top', -y);
-				
+*/				
 				e.preventDefault();
 			}
 		},			
@@ -86,6 +103,38 @@ sample('Пользовательский скроллбар', {
 //				growl.info(x);
 				
 				this.scrollbar.content.el.height(Math.max(x*h, 10));
+			},
+			scrollContent: function(ratio) {
+				
+				// получаем границы, в которых должно умещаться смещение
+				var min_y = 0;
+				var max_y = this.content.el.outerHeight() - this.el.height();
+				
+				var y = max_y * ratio;
+				
+				// нормализуем смещение
+				y = Math.max(min_y, y);
+				y = Math.min(max_y, y);
+				
+				// устанавливаем смещение содержимого
+				this.content.el.css('margin-top', -y);
+				
+			},
+			scrollScrollbar: function(ratio) {
+
+				// получаем границы, в которых должно умещаться смещение
+				var min_y = 0;
+				var max_y = this.scrollbar.el.height() - this.scrollbar.content.el.outerHeight();
+
+				var y = max_y * ratio;
+				
+				// нормализуем смещение
+				y = Math.max(min_y, y);
+				y = Math.min(max_y, y);
+				
+				// устанавливаем смещение содержимого
+				this.scrollbar.content.el.css('margin-top', y);
+				
 			}
 		}],
 		
