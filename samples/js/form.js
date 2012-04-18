6c217bc69ebea9eb96794b04bfdebe15ea6a69d4
Коданев Юрий <eliace.the.grey@gmail.com>
2012-04-18 10:12:27 +0400

    + slider-sample

--- samples/js/form.js
+++ samples/js/form.js
@@ -207,6 +207,48 @@ var w = sample('Форма с ergo-виджетами', {
 		}
 		
 		
+	}, {
+		cls: 'e-slider-holder',
+		content: {
+			cls: 'e-slider',
+			content: {
+				cls: 'e-slider-roller',
+				content: {
+					html: '<span/>'
+				},
+				events: {
+					'mousedown': function(e, w) {
+						
+						// сохраняем смещение курсора относительно начала ползунка
+						w._dx = e.pageX - w.parent.el.offset().left - parseInt(w.el.css('margin-left').replace("px", ""));
+						
+						$(window)
+							.one('mouseup', function(e){
+								// отключаем слежение за перемещением мыши
+								$(window).off('mousemove');
+							})
+							.on('mousemove', function(e){
+								var x = e.pageX - w.parent.el.offset().left - w._dx;
+								
+								var max_x = w.parent.el.width() - w.el.outerWidth();
+								
+								var val = x / max_x;
+								
+								val = Math.max(0, val);
+								val = Math.min(1.0, val);
+								
+								w.el.css({'margin-left': val*max_x});
+								
+								w.events.bubble('slide', {value: val});
+							})
+							
+						e.preventDefault();	
+					}
+				}
+			}			
+		}
+		
+		
 	}]
 	
 	
