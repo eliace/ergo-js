5be27a59e4fafdf6743a4fe789e8c4c051ab6c0f
Коданёв Юрий <yukodanev@itgroup.lc>
2012-10-18 13:58:42 +0400

    ~

---
+++ js/widgets/main-menu.js
@@ -0,0 +1,35 @@
+
+//= require <widgets/natives/list>
+
+
+Ergo.declare('Ergo.widgets.MainMenu', 'Ergo.widgets.List', {
+	
+	defaults: {
+		etype: 'list',
+		cls: 'e-main-menu',
+		mixins: ['selectable'],
+		components: {
+			logo: {
+				etype: 'box',
+				weight: -10,
+				cls: 'logo',
+				content: {
+					etype: 'image'
+//					src: 'samples/img/logo.png'
+				}
+			}
+		},
+		defaultItem: {
+			onClick: function() {
+				this.parent.selection.set(this);
+			}
+		}
+	},
+	
+	
+	setLogo: function(v) {
+		this.logo.content.opt('src', v);
+	}
+	
+	
+}, 'main-menu');
