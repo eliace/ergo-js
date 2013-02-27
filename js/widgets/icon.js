e38efa024831105fc21cb25a2a2e475e1afbb9c4
Коданев Юрий Валерьевич <YuKodanev@itgroup.lc>
2013-02-27 13:24:14 +0400

    ~

--- js/widgets/icon.js
+++ js/widgets/icon.js
@@ -8,6 +8,11 @@ Ergo.declare('Ergo.widgets.Icon', 'Ergo.widgets.Box', {
 	defaults: {
 		cls: 'e-icon'
 //		html: '<div>icon</div>'
+	},
+	
+	
+	setMode: function(v) {
+		this.states.toggle('e-mode-'+v, 'e-mode-');
 	}
 	
 }, 'icon');
