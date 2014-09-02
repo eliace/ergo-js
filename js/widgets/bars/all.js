f0e52b8d704ca987b01a9417bd1ab2dc3d62e048
unknown <YuKodanev@SYKAV-WS-LT-021.domain.local>
2014-09-02 12:33:15 +0400

    ~

--- js/widgets/bars/all.js
+++ js/widgets/bars/all.js
@@ -1,3 +1,4 @@
 
 //= require "tab-bar"
 //= require "tool-bar"
+//= require "menu-bar"
---
+++ js/widgets/bars/menu-bar.js
@@ -0,0 +1,12 @@
+
+
+Ergo.defineClass('Ergo.widgets.MenuBar', 'Ergo.widgets.List', {
+	
+	defaults: {		
+		baseCls: 'menu-bar',
+		defaultItem: {
+		}				
+	}
+	
+	
+}, 'widgets:menu-bar');
