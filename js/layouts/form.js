3496e10dc27785772cdf4afc4c0f6ab9fc63bd2d
unknown <YuKodanev@SYKAV-WS-LT-021.domain.local>
2015-04-15 09:31:43 +0300

    ~

--- js/layouts/form.js
+++ js/layouts/form.js
@@ -8,8 +8,12 @@ Ergo.declare('Ergo.layouts.Form', 'Ergo.core.Layout', {
 	
 	wrap: function(item) {
 		var w = $('<div/>');
-		if(item.label)
+		if(!item.label && item.options.label) {
+			item.label = $.ergo({etype: 'html:label', text: item.options.label});
+		}
+		if(item.label) {
 			w.append(item.label.el);
+		}
 		w.append(item.el);
 		return w;
 	}
