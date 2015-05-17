ecd2f5802acdd0bfe9dc177c7ddeaa3fab71b28b
eliace <eliace@mail.ru>
2015-05-17 11:37:23 +0300

    clean

---
+++ js/mixins/icon-addon.js
@@ -0,0 +1,24 @@
+
+
+Ergo.defineMixin('Ergo.mixins.IconAddon', function(o) {
+
+	o.components = Ergo.smart_override({
+		addon: {
+			cls: 'box',
+			weight: 10,
+			cls: 'addon',
+			components: {
+				icon: {
+					etype: 'icon'
+//					cls: 'fa fa-fw'
+				}
+			}
+		}
+	}, o.components);
+
+	this.set_icon = function(v) {
+		this.addon.icon.opt('text', v);
+	}
+
+
+}, 'mixins:icon-addon');
